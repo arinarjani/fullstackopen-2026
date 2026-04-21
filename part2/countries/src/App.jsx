c98c67654298f64c5955f3948d3977e6af28b807
arinarjani <arinrjani@gmail.com>
2026-04-21 11:30:43 -0700

    started working on part 3

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
-  const [allCountires, setAllCountries] = useState(null)
+  const [allCountries, setAllCountries] = useState(null)
   const [search, setSearch] = useState('')
   const [returnedSearch, setReturnedSearch] = useState(null)
 
@@ -27,31 +27,42 @@ function App() {
   const handleSearch = (e) => {
     setSearch(e.target.value)
 
-    
+    setReturnedSearch(allCountries.filter(c => c.name.common.toLowerCase() === e.target.value.toLowerCase()))
 
-    // setReturnedSearch(allCountires.filter(c => c.name.common.toLowerCase() === 'palestine'))
+    console.log('search term: ', search)
+    console.log('countries that match search term: ', 
+                allCountries.filter(c => c.name.common.toLowerCase() === search.toLowerCase())
+    )
+    
+    // setReturnedSearch(allCountries.filter(c => c.name.common.toLowerCase() === 'palestine'))
 
     // console.log('returnedSearch - ', returnedSearch)
 
-    // console.log(`results for ${search}: `, allCountires.filter(c => {
+    // console.log(`results for ${search}: `, allCountries.filter(c => {
     //   c.name.common.toLowerCase() === search.toLowerCase()
     // }))
   }
 
-  if (allCountires !== null ) {
-    
-    console.log(allCountires.filter(c => c.name.common.toLowerCase() === 'switzerland'))
+  // if (allCountries !== null ) {
+  //   // log out the country that matches the search term (if there is one)
+  //   console.log('search term: ', search)
+  //   console.log('countries that match search term: ', 
+  //               allCountries.filter(c => c.name.common.toLowerCase() === search.toLowerCase())
+  //   )
+  // }
 
-    // allCountires.map(c => {
+
+    //console.log(allCountries.filter(c => c.name.common.toLowerCase() === 'switzerland'))
+
+    // allCountries.map(c => {
     //   console.log(c.name.common.toLowerCase() === 'palestine')
     // })
-    // console.log(allCountires[21].name.common.toLowerCase())
-  }
+    // console.log(allCountries[21].name.common.toLowerCase())
 
   return (
     <>
     {
-      allCountires === null ? 'loading api data...' : 
+      allCountries === null ? 'loading api data...' : 
       <form>
         <div>
           <label htmlFor="country">find countries</label>
@@ -60,6 +71,27 @@ function App() {
       </form>
     }
     {
+      returnedSearch !== null && returnedSearch.length > 0 ?
+        // loop through returnedSearch and show the details of the country if the letters are included in the earch term
+        // example: if I search for 'sw' it should show me Switzerland, Sweden, and any other country that 
+        // has 'sw' in its name (so I can find the country even if I don't know how to spell it correctly   
+        returnedSearch.map(c => (
+        <div key={c.name.common}>
+          <h1>{c.name.common}</h1>
+          <p>Capital(s): {c.capital.map(c => c)}</p>
+          <p>Area: {c.area}</p>
+          <h2>Languages</h2>
+          <ul>
+            {
+              Object.entries(c.languages).map(([key, value]) => <li>{`${value}`}</li>)
+            }
+          </ul>
+          <img src={c.flags.png} alt={`The flag of ${c.name.common} is ${c.flags.alt}`} />
+        </div>
+      ))
+      : search !== '' ? 'no results found' : 'search results here'
+    }
+    {/* {
       search !== '' ?
         // allCountires.filter(c => c.name.common.toLowerCase() === search.toLowerCase()).map(name => <p>{name.name.common}</p>)
         allCountires.filter(c => c.name.common.toLowerCase() === search.toLowerCase()).map(c => (
@@ -79,7 +111,7 @@ function App() {
         // returnedSearch.map(name => <p>{name.name.common}</p>)
       : 
       <p>search results here</p>
-    }
+    } */}
     </>
   )
 }
